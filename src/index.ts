38e8e9c4b86f1f60aa98c91c07e60e4fd9db0a70
emiiia <emilia24k@gmail.com>
2023-08-18 16:11:16 +0100

    Initial getEvolutionChain

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
-const getEvolutionChain = async (pokemonName: string) => {
-  console.log(pokemonName);
+const axios = require("axios");
+
+const parseChain: any = (chain: any) => {
+  let variations = [];
+  // Pokemon can have multiple variations for the next evolution (e.g. Eevee)
+  for (let i = 0; i < chain.evolves_to.length; i++) {
+    // For each possible next evolution, get the name and variations
+    variations.push(parseChain(chain.evolves_to[i]));
+  }
+
+  return {
+    name: chain.species.name,
+    variations,
+  };
 };
 
-getEvolutionChain("ludicolo");
+const getEvolutionChain = async (pokemonName: string) => {
+  try {
+    const responseSpecies = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon-species/${pokemonName.toLowerCase()}`
+    );
+
+    // Get the url for the evolution chain API
+    const evolutionChainAPI = responseSpecies.data["evolution_chain"]["url"];
+
+    const responseEvolution = await axios.get(evolutionChainAPI);
+
+    // Get the chain JSON
+    const chain = responseEvolution.data["chain"];
+
+    // Parse the response into a format showing names and variations
+    const evolutionChain = parseChain(chain);
+
+    const formattedChain = JSON.stringify(evolutionChain, null, 4);
+
+    console.log(formattedChain);
+
+    return formattedChain;
+  } catch (error) {
+    console.error(`Error fetching data for ${pokemonName}: ${error}`);
+    return null;
+  }
+};
+
+getEvolutionChain("eevee");
 
 module.exports = getEvolutionChain;
